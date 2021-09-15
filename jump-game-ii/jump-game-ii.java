3a6671b433b96f17fbc5f090d90ad4da80e48db5
shyam0705 <72148710+shyam0705@users.noreply.github.com>
2021-09-15 16:42:13 +0530

    Time: 1 ms (99.49%), Space: 39.8 MB (57.82%) - LeetHub

--- jump-game-ii/jump-game-ii.java
+++ jump-game-ii/jump-game-ii.java
@@ -1,42 +1,27 @@
 class Solution {
-    int[] dp;
-    public int recur(int[] nums,int i,int n)
-    {
-        if(i==n-1)
+    public int jump(int[] nums) {
+        int n=nums.length;
+        if(n==1)
         {
             return 0;
         }
-        if(i+nums[i]>=(n-1))
+        int max_end=nums[0];
+        int count=0;
+        int max=Integer.MIN_VALUE;
+        for(int i=1;i<n;i++)
         {
-            return 1;
-        }
-        if(dp[i]!=-2)
-        {
-            return dp[i];
-        }
-        int ans=Integer.MAX_VALUE;
-        for(int j=1;j<=nums[i];j++)
-        {
-            int tmp=recur(nums,i+j,n);
-            if(tmp!=-1)
+            if(i>max_end)
             {
-                ans=Math.min(tmp,ans);
+                count=count+1;
+                max_end=max;
+                max=i+nums[i];
+            }
+            else
+            {
+               max=Math.max(max,i+nums[i]);
             }
         }
-        if(ans==Integer.MAX_VALUE)
-        {
-            dp[i]=-1;
-            return -1;
-        }
-        dp[i]=1+ans;
-        return 1+ans;
-    }
-    public int jump(int[] nums) {
-        dp=new int[nums.length];
-        for(int i=0;i<nums.length;i++)
-        {
-            dp[i]=-2;
-        }
-        return recur(nums,0,nums.length);
+        return count+1;
+        
     }
 }
