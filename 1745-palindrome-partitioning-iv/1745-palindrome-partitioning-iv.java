415581579ff083c22c8db59f4fc87c601a2b4d51
Shyam Patel <72148710+shyam0705@users.noreply.github.com>
2022-01-05 18:27:58 +0530

    Time: 135 ms (37.17%), Space: 41.5 MB (97.34%) - LeetHub

---
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.java
@@ -0,0 +1,58 @@
+class Solution {
+    public boolean checkPartitioning(String s) {
+        int n=s.length();
+        boolean[][] dp=new boolean[n+1][n];
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=0;j<n-i+1;j++)
+            {
+                if(i==1)
+                {
+                    dp[i][j]=true;
+                }
+                else
+                {
+                    if(s.charAt(j)==s.charAt(j+i-1))
+                    {
+                        if(i!=2)
+                        {
+                            dp[i][j]=dp[i-2][j+1];
+                        }
+                        else
+                        {
+                            dp[i][j]=true;
+                        }
+                        
+                        
+                    }
+                    else
+                    {
+                        dp[i][j]=false;
+                    }
+                }
+                // System.out.println(dp[i][j]);
+            }
+        }
+        boolean[][] ans=new boolean[n][3];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                if(dp[i-j+1][j])
+                {
+                    if(j==0)
+                    {
+                        ans[i][0]=true;
+                    }
+                    else
+                    {
+                        ans[i][2]=ans[i][2] | ans[j-1][1];
+                        ans[i][1]=ans[i][1] | ans[j-1][0];
+                    }
+                    
+                }
+            }
+        }
+        return ans[n-1][2];
+    }
+}
