1421ab797da7376669c23b55bce46562f9cc2093
Shyam Patel <72148710+shyam0705@users.noreply.github.com>
2022-01-26 10:42:19 +0530

    Time: 32 ms (27.59%), Space: 84.7 MB (42.03%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -14,6 +14,83 @@
  * }
  */
 class Solution {
+    public List<Integer> optimal(TreeNode root1,TreeNode root2)
+    {
+        TreeNode current1=root1;
+        TreeNode current2=root2;
+        Stack<TreeNode>s1=new Stack<>();
+        Stack<TreeNode>s2=new Stack<>();
+        List<Integer>ans=new ArrayList<>();
+        while(true)
+        {
+            if(current1==null && current2==null)
+            {
+                if(s1.isEmpty() && s2.isEmpty())
+                {
+                    break;
+                }
+                else if(s1.isEmpty())
+                {
+                    TreeNode tmp=s2.pop();
+                    ans.add(tmp.val);
+                    if(tmp.right!=null)
+                    {
+                        current2=tmp.right;
+                    }
+                }
+                else if(s2.isEmpty())
+                {
+                    TreeNode tmp=s1.pop();
+                    ans.add(tmp.val);
+                    if(tmp.right!=null)
+                    {
+                        current1=tmp.right;
+                    }
+                }
+                else
+                {
+                    TreeNode tmp1=s1.peek();
+                    TreeNode tmp2=s2.peek();
+                    if(tmp1.val<tmp2.val)
+                    {
+                        TreeNode tmp=s1.pop();
+                        ans.add(tmp.val);
+                        if(tmp.right!=null)
+                        {
+                            current1=tmp.right;
+                        } 
+                    }
+                    else
+                    {
+                        TreeNode tmp=s2.pop();
+                        ans.add(tmp.val);
+                        if(tmp.right!=null)
+                        {
+                            current2=tmp.right;
+                        }
+                    }
+                }
+            }
+            else if(current1==null)
+            {
+                s2.add(current2);
+                current2=current2.left;
+            }
+            else if(current2==null)
+            {
+               s1.add(current1);
+                current1=current1.left;
+            }
+            else
+            {
+                s1.add(current1);
+                s2.add(current2);
+                current1=current1.left;
+                current2=current2.left;
+            }
+        }
+        return ans;
+    }
     public List<Integer> inorder(TreeNode root)
     {
         TreeNode current=root;
@@ -45,11 +122,7 @@ class Solution {
         }
         return ans;
     }
-    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        List<Integer>arr1=inorder(root1);
-        List<Integer>arr2=inorder(root2);
-        return merge(arr1,arr2);
-    }
+    
     public List<Integer> merge(List<Integer>arr1,List<Integer>arr2)
     {
         int n=arr1.size();
@@ -83,4 +156,11 @@ class Solution {
         return ans;
         
     }
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        // List<Integer>arr1=inorder(root1);
+        // List<Integer>arr2=inorder(root2);
+        // return merge(arr1,arr2);
+        return optimal(root1,root2);
+    }
+    
 }
