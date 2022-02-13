36a1e4b053319651fb64c3e58dffc4e4508e13a3
Shyam Patel <72148710+shyam0705@users.noreply.github.com>
2022-02-13 12:15:56 +0530

    Time: 2 ms (44.88%), Space: 42.8 MB (19.59%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,21 +1,35 @@
 class Solution {
     List<List<Integer>>ans;
-    public void recur(int[] nums,int i,List<Integer>current,int n)
-    {
-        if(i==n)
-        {
-            ans.add(new ArrayList<Integer>(current));
-            return;
-        }
-        recur(nums,i+1,current,n);
-        current.add(nums[i]);
-        recur(nums,i+1,current,n);
-        current.remove(current.size()-1);
-    }
+    // public void recur(int[] nums,int i,List<Integer>current,int n)
+    // {
+    //     if(i==n)
+    //     {
+    //         ans.add(new ArrayList<Integer>(current));
+    //         return;
+    //     }
+    //     recur(nums,i+1,current,n);
+    //     current.add(nums[i]);
+    //     recur(nums,i+1,current,n);
+    //     current.remove(current.size()-1);
+    // }
     public List<List<Integer>> subsets(int[] nums) {
         ans=new ArrayList<>();
         int n=nums.length;
-        recur(nums,0,new ArrayList<>(),n);
+        int k=(1<<n)-1;
+        while(k>=0)
+        {
+            List<Integer>current=new ArrayList<>();
+            for(int i=0;i<32;i++)
+            {
+                int tmp=(k>>i)&1;
+                if(tmp==1)
+                {
+                    current.add(nums[i]);
+                }
+            }
+            ans.add(current);
+            k--;
+        }
         return ans;
     }
 }
