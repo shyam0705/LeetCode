ff816522f486ac3008d25dcc4e0837f95d08f602
Shyam Patel <72148710+shyam0705@users.noreply.github.com>
2022-08-22 11:38:43 +0530

    Time: 1 ms (100.00%), Space: 41.8 MB (19.85%) - LeetHub

--- 342-power-of-four/342-power-of-four.java
+++ 342-power-of-four/342-power-of-four.java
@@ -1,23 +1,10 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if((n & (n-1))==0)
+        if(n==0)
+            return false;
+        if((n & (n-1))==0 && Math.sqrt(n)==(int)Math.sqrt(n))
         {
-             //System.out.println("hello");
-            int tmp=1;
-            for(int i=0;i<32;i++)
-            {
-                
-               // System.out.println(n&tmp);
-                if((n&tmp)!=0)
-                {
-                   
-                    if(i%2==0)
-                        return true;
-                    return false;
-                    
-                }
-                tmp=tmp<<1;
-            }
+             return true;
         }
         return false;
     }
