d14b507bf1658ec326cde049a39b2c6caee2e383
Shyam Patel <72148710+shyam0705@users.noreply.github.com>
2022-10-07 13:06:26 +0530

    Time: 2 ms (84.40%), Space: 40.3 MB (91.25%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.java
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.java
@@ -2,18 +2,14 @@ class Solution {
     public int arithmeticTriplets(int[] nums, int diff) {
         int n=nums.length;
         int count=0;
+        HashMap<Integer,Integer>mp=new HashMap<>();
         for(int i=0;i<n;i++)
         {
-            for(int j=i+1;j<n;j++)
+            if(mp.containsKey(nums[i]-diff) && mp.containsKey(nums[i]-2*diff))
             {
-                for(int k=j+1;k<n;k++)
-                {
-                    if(nums[j]-nums[i]==diff && nums[k]-nums[j]==diff)
-                    {
-                        count++;
-                    }
-                }
+               count=count+Math.min(mp.get(nums[i]-diff),mp.get(nums[i]-2*diff));
             }
+            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
         }
         return count;
     }
