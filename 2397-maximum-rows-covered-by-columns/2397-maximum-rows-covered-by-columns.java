7c43753bd9b972ebd8a68f1e59b5f83a6070b2f4
Shyam Patel <72148710+shyam0705@users.noreply.github.com>
2022-10-10 11:07:19 +0530

    Time: 150 ms (5.04%), Space: 112.1 MB (5.10%) - LeetHub

--- 2397-maximum-rows-covered-by-columns/2397-maximum-rows-covered-by-columns.java
+++ 2397-maximum-rows-covered-by-columns/2397-maximum-rows-covered-by-columns.java
@@ -1,5 +1,6 @@
 class Solution {
     int n;
+    int[][] dp;
     public int recur(int[][] matrix,int k,int index,int m,int num)
     {
         if(index==m)
@@ -22,11 +23,18 @@ class Solution {
             }
             return count;
         }
+        
+        if(dp[index][num]!=-1)
+        {
+            return dp[index][num];
+        }
         int tmp=0;
         if(k>0)
             tmp=recur(matrix,k-1,index+1,m,num+(1<<index));
         
         tmp=Math.max(tmp,recur(matrix,k,index+1,m,num));
+        
+        dp[index][num]=tmp;
         return tmp;
         
         
@@ -34,6 +42,15 @@ class Solution {
     public int maximumRows(int[][] matrix, int numSelect) {
         n=matrix.length;
         int m=matrix[0].length;
+        
+        dp=new int[m+1][100000];
+        for(int i=0;i<=m;i++)
+        {
+            for(int j=0;j<100000;j++)
+            {
+                dp[i][j]=-1;
+            }
+        }
         return recur(matrix,numSelect,0,m,0);
     }
 }
